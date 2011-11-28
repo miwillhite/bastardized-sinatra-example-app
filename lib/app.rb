d2fcd7b71a754bd36215097b6fcfb9e9d796582c
Matt Willhite <mwillhite@elctech.com>
2011-11-28 11:19:50 -0800

    Added public folder definition

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,9 @@
 %w[rubygems sinatra/base].each { |dep| require dep }
 
-
 class AppFace < Sinatra::Base
+  
+  set :public_folder, File.expand_path(File.join(File.dirname(__FILE__), 'public'))
+
   get '/' do
     File.read File.join('public', 'index.html')
   end
